cb9a4191c914131f00feff6d8a29abe51c2a8e87
Reiner Marquez <rmaceissoft@gmail.com>
2021-04-20 19:23:44 -0500

    refactor parse_date and make value param optional

--- pyjne_peru/utils.py
+++ pyjne_peru/utils.py
@@ -2,13 +2,14 @@ from datetime import datetime, date
 from typing import Optional
 
 
-def parse_date(value: str, format="%d/%m/%Y") -> Optional[date]:
-    if not value:
-        return None
-    return datetime.strptime(value, format).date()
-
-
-def parse_datetime(value: str, format="%d/%m/%Y %H:%M:%S") -> Optional[datetime]:
+def parse_datetime(value: Optional[str], format="%d/%m/%Y %H:%M:%S") -> Optional[datetime]:
     if not value:
         return None
     return datetime.strptime(value.replace('.', ''), format)
+
+
+def parse_date(value: Optional[str], format="%d/%m/%Y") -> Optional[date]:
+    result = parse_datetime(value, format=format)
+    if not result:
+        return None
+    return result.date()
